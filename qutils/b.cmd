c64c21f94a84434030911c066a90d70cdde30fd2
Péter Györök <gyorokpeter1@gmail.com>
2020-09-12 22:54:27 +0100

    depend on KxSystems/kdb

--- qutils/b.cmd
+++ qutils/b.cmd
@@ -1,2 +1,3 @@
 cls
-g++ -shared qutils.cpp qutilsk.cpp -I.. -L.. -lq -o ../qutils.dll
+call config.cmd
+g++ -shared qutils.cpp qutilsk.cpp -I%KX_KDB_PATH%/c/c -L%KX_KDB_PATH%/w32 -lq -o ../qutils.dll
