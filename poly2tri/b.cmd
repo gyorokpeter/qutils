3f6c2c3bcf6f081080b3c4bffccc813b707f9775
gyorokpeter <gyorokpeter@freemail.hu>
2020-09-13 18:03:43 +0100

    update build file for poly2tri

--- poly2tri/b.cmd
+++ poly2tri/b.cmd
@@ -1,2 +1,13 @@
 cls
-g++ -o poly2tri2.dll -shared poly2tri_k.cpp shapes.o cdt.o sweep.o sweep_context.o advancing_front.o -I.. -ID:/Projects/github/poly2tri/poly2tri -L.. -lq
+@call config.cmd
+@if not exist ..\libq.a (
+	echo create ..\libq.a as per https://code.kx.com/q/interfaces/using-c-functions/#windows-mingw-64
+	exit 1
+)
+g++ -o ../poly2tri2.dll -shared poly2tri_k.cpp ^
+	%POLY2TRI_PATH%/poly2tri/common/shapes.cc ^
+	%POLY2TRI_PATH%/poly2tri/sweep/cdt.cc ^
+	%POLY2TRI_PATH%/poly2tri/sweep/sweep.cc ^
+	%POLY2TRI_PATH%/poly2tri/sweep/sweep_context.cc ^
+	%POLY2TRI_PATH%/poly2tri/sweep/advancing_front.cc ^
+	-I.. -I%POLY2TRI_PATH%/poly2tri -I%KX_KDB_PATH%/c/c -L.. -lq
